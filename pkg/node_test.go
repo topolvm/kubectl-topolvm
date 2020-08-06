a2119f0310a860033b512a392784c9eceeb2942b
moricho <ikeda.morito@gmail.com>
2020-08-06 05:13:44 +0000

    add test case for summarize

--- pkg/node_test.go
+++ pkg/node_test.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"context"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/topolvm/topolvm"
@@ -40,11 +41,38 @@ var _ = Describe("node command test", func() {
 						available:    100,
 						usedRate:     60,
 					},
+					{
+						nodeName:     "test1",
+						devClassName: "hdd",
+						used:         100,
+						available:    200,
+						usedRate:     33,
+					},
+					{
+						nodeName:     "test1",
+						devClassName: "00default",
+						used:         0,
+						available:    100,
+						usedRate:     0,
+					},
+					{
+						nodeName:     "test2",
+						devClassName: "ssd",
+						used:         100,
+						available:    10,
+						usedRate:     90,
+					},
+					{
+						nodeName:     "test2",
+						devClassName: "00default",
+						used:         0,
+						available:    10,
+						usedRate:     0,
+					},
 				}
 				nodeInfos, err := summarize(context.Background(), k8sClient)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(nodeInfos).Should(HaveLen(5))
-				Expect(nodeInfos).Should(ContainElements(expectedNodeInfos))
+				Expect(nodeInfos).Should(ConsistOf(expectedNodeInfos))
 			})
 		})
 		When("node annotation var is zero", func() {
@@ -67,6 +95,70 @@ var _ = Describe("node command test", func() {
 				nodeInfos, err := summarize(context.Background(), k8sClient)
 				Expect(err).ToNot(HaveOccurred())
 				Expect(nodeInfos).Should(HaveLen(3))
+				Expect(nodeInfos).Should(ContainElements(expectedNodeInfos))
+			})
+		})
+		When("device class is empty", func() {
+			It(`should assign "00default"`, func() {
+				createNode("test1", map[string]string{
+					"00default": "100",
+				})
+				createLv("lv1", "test1", "", 100)
+				expectedNodeInfos := []nodeInfo{
+					{
+						nodeName:     "test1",
+						devClassName: "00default",
+						used:         100,
+						available:    100,
+						usedRate:     50,
+					},
+				}
+				nodeInfos, err := summarize(context.Background(), k8sClient)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nodeInfos).Should(HaveLen(1))
+				Expect(nodeInfos).Should(ContainElements(expectedNodeInfos))
+			})
+		})
+		When("the node name which does not exist is used", func() {
+			It(`should be ignored`, func() {
+				createNode("test1", map[string]string{
+					"ssd":       "100",
+					"00default": "100",
+				})
+				createLv("lv1", "not_exist", "ssd", 100)
+				expectedNodeInfos := []nodeInfo{
+					{
+						nodeName:     "test1",
+						devClassName: "ssd",
+						used:         0,
+						available:    100,
+						usedRate:     0,
+					},
+				}
+				nodeInfos, err := summarize(context.Background(), k8sClient)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nodeInfos).Should(HaveLen(2))
+				Expect(nodeInfos).Should(ContainElements(expectedNodeInfos))
+			})
+		})
+		When("the device class name which does not exist is used", func() {
+			It(`should be ignored`, func() {
+				createNode("test1", map[string]string{
+					"ssd": "100",
+				})
+				createLv("lv1", "test1", "not_exist", 100)
+				expectedNodeInfos := []nodeInfo{
+					{
+						nodeName:     "test1",
+						devClassName: "ssd",
+						used:         0,
+						available:    100,
+						usedRate:     0,
+					},
+				}
+				nodeInfos, err := summarize(context.Background(), k8sClient)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nodeInfos).Should(HaveLen(1))
 				Expect(nodeInfos).Should(ContainElements(expectedNodeInfos))
 			})
 		})
