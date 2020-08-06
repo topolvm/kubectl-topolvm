84d2cbc484ed210214acee08f0738d78af1bc597
chez-shanpu <cheztomo513@gmail.com>
2020-08-06 11:58:23 +0900

    Add test case

--- pkg/node_test.go
+++ pkg/node_test.go
@@ -2,7 +2,6 @@ package pkg
 
 import (
 	"context"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/topolvm/topolvm"
@@ -14,40 +13,76 @@ import (
 
 var _ = Describe("node command test", func() {
 	Context("summarize success case", func() {
-		It("create test data", func() {
-			createNode("test1", map[string]string{
-				"ssd":       "100",
-				"00default": "100",
-				"hdd":       "200",
-			})
-			createNode("test2", map[string]string{
-				"ssd":       "10",
-				"00default": "10",
-			})
-			createLv("lv1", "test1", "ssd", 50)
-			createLv("lv2", "test1", "ssd", 100)
-			createLv("lv3", "test1", "hdd", 100)
-			createLv("lv4", "test2", "ssd", 50)
-			createLv("lv5", "test2", "ssd", 50)
+		AfterEach(func() {
+			clearResources()
 		})
-		It("should summarize node info", func() {
-			expectedNodeInfos := []nodeInfo{
-				{
-					nodeName:     "test1",
-					devClassName: "ssd",
-					used:         150,
-					available:    100,
-					usedRate:     60,
-				},
-			}
-			nodeInfos, err := summarize(context.Background(), k8sClient)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(nodeInfos).Should(HaveLen(5))
-			Expect(nodeInfos).Should(ContainElements(expectedNodeInfos))
+		When("all ok", func() {
+			It("should summarize node info", func() {
+				createNode("test1", map[string]string{
+					"ssd":       "100",
+					"00default": "100",
+					"hdd":       "200",
+				})
+				createNode("test2", map[string]string{
+					"ssd":       "10",
+					"00default": "10",
+				})
+				createLv("lv1", "test1", "ssd", 50)
+				createLv("lv2", "test1", "ssd", 100)
+				createLv("lv3", "test1", "hdd", 100)
+				createLv("lv4", "test2", "ssd", 50)
+				createLv("lv5", "test2", "ssd", 50)
+				expectedNodeInfos := []nodeInfo{
+					{
+						nodeName:     "test1",
+						devClassName: "ssd",
+						used:         150,
+						available:    100,
+						usedRate:     60,
+					},
+				}
+				nodeInfos, err := summarize(context.Background(), k8sClient)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nodeInfos).Should(HaveLen(5))
+				Expect(nodeInfos).Should(ContainElements(expectedNodeInfos))
+			})
+		})
+		When("node annotation var is zero", func() {
+			It(`should output "used ratio" is zero`, func() {
+				createNode("test1", map[string]string{
+					"ssd":       "100",
+					"00default": "100",
+					"hdd":       "0",
+				})
+				createLv("lv1", "test1", "hdd", 0)
+				expectedNodeInfos := []nodeInfo{
+					{
+						nodeName:     "test1",
+						devClassName: "hdd",
+						used:         0,
+						available:    0,
+						usedRate:     0,
+					},
+				}
+				nodeInfos, err := summarize(context.Background(), k8sClient)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nodeInfos).Should(HaveLen(3))
+				Expect(nodeInfos).Should(ContainElements(expectedNodeInfos))
+			})
 		})
 	})
-	Context("confirming summarize()", func() {
-
+	Context("summarize failure case", func() {
+		When("node annotation var is string", func() {
+			It("returns error", func() {
+				createNode("test1", map[string]string{
+					"ssd":       "aaa",
+					"00default": "aaa",
+				})
+				createLv("lv1", "test1", "ssd", 50)
+				_, err := summarize(context.Background(), k8sClient)
+				Expect(err).To(HaveOccurred())
+			})
+		})
 	})
 })
 
@@ -82,5 +117,12 @@ func createLv(name, nodeName, devClass string, size int64) {
 	err := k8sClient.Create(ctx, &lv)
 	Expect(err).ToNot(HaveOccurred())
 	err = k8sClient.Status().Update(ctx, &lv)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func clearResources() {
+	err := k8sClient.DeleteAllOf(context.Background(), &corev1.Node{})
+	Expect(err).ToNot(HaveOccurred())
+	err = k8sClient.DeleteAllOf(context.Background(), &topolvmv1.LogicalVolume{})
 	Expect(err).ToNot(HaveOccurred())
 }
