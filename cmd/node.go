3bdc2d95ff5ad0076723d89b50ad2ecb166da5cf
Tomoki Sugiura <30888767+chez-shanpu@users.noreply.github.com>
2020-08-13 13:33:05 +0900

    Update cmd/node.go
    Co-authored-by: Yuji Ito <llamerada-jp@users.noreply.github.com>

--- cmd/node.go
+++ cmd/node.go
@@ -9,7 +9,7 @@ import (
 var nodeCmd = &cobra.Command{
 	Use:   "node",
 	Short: "Show information about TopoLVM logical volume for each node",
-	Long:  "Show information about TopoLVM logical volume for each node",
+	Long:  "Show information about TopoLVM logical volume for each node.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return pkg.PrintNodesInfo(cmd.Context(), kubeClient)
 	},
